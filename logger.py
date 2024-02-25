82d2468059038e44643db63c56bdd360241a23aa
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2024-02-25 12:45:44 +0100

    Add logging enable/disable functions

--- logger.py
+++ logger.py
@@ -2,3 +2,9 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+
+def enable_logging():
+    logger.setLevel(logging.INFO)
+
+def disable_logging():
+    logger.setLevel(logging.CRITICAL) # Disable all logging
