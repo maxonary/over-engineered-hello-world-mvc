2f5ff90fd85463ddba67a0c4d350d02ce0bf0d35
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2024-02-25 12:45:47 +0100

    Add logging functionality to main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from controller import start_cypher
 from config import CypherConfig
+from logger import enable_logging, disable_logging
 
-if __name__ == "__main__":
+def main():
+    user_choice_logger = input("Would you like to enable logging? (y/n): ").strip().lower()
+    if user_choice_logger == 'y':
+        enable_logging()
+    else:
+        disable_logging()
+
     config = CypherConfig(min_shift=1, max_shift=9)
     start_cypher(config)
+
+if __name__ == "__main__":
+    main()
